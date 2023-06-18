527de888440a8261c962bca072ecf6f652a54ee4
Chris M <chris@chrismabey.dev>
2023-06-18 13:11:02 -0300

    express server, image generation route, openai controller

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+// Init server
+const app = express();
+// Body Parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+// Cors Middleware
+app.use(cors({ 
+  origin: ['http://localhost:3000', 'http://localhost:3001'], 
+  credentials: true 
+}));
+// Welcome Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Ai Image Generation App!');
+});
+// AI Image Generation Route
+app.use('/openai', require('./routes/openaiRoutes.js'));
+
+// Start server
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
